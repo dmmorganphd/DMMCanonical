68e0cf37a08ca9048e93a6823480269ade3b7270
dmmorganphd <65974829+dmmorganphd@users.noreply.github.com>
2024-10-03 20:47:48 -0500

    in the midst of vetting the scientific computing html

---
+++ create_tgz_archive.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+wd=`pwd`
+parentdir="$(dirname "$wd")"
+cd $parentdir
+if [ $? != 0 ]; then
+	echo "problem changing to parent directory"
+	exit
+fi
+
+IFS='/'
+read -ra newarr <<< $wd
+tfn=${newarr[-1+${#newarr[@]}]}".tgz"
+tar -zcvf $tfn ${newarr[-1+${#newarr[@]}]} 
